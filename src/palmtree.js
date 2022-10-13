8f4d07c32847062aa142c8bbb72ae1cd5a8e9352
sophie <zvava@disroot.org>
2022-10-13 14:59:16 +0100

    *

--- src/palmtree.js
+++ src/palmtree.js
@@ -4,6 +4,6 @@ addEventListener("load", () => {
 	nav.style.background = "rgba(0, 0, 0, 0)"
 
 	addEventListener("scroll", () => {
-		nav.style.background = scrollY > 0 ? "rgba(0, 0, 0, .25)" : "rgba(0, 0, 0, 0)"
+		nav.style.background = scrollY > 0 ? "rgba(0, 0, 0, .33)" : "rgba(0, 0, 0, 0)"
 	})
 })
