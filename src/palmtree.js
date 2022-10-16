e74f6d6e7e5f63979197088ebf0c811e744354de
sophie <zvava@disroot.org>
2022-10-16 23:51:26 +0100

    finna fix sticking footer to bottom

--- src/palmtree.js
+++ src/palmtree.js
@@ -1,7 +1,6 @@
 addEventListener("load", () => {
 	const graphic = document.getElementsByTagName("header")[0]
 	const nav = document.getElementsByTagName("nav")[0]
-	nav.style.background = "rgba(0, 0, 0, 0)"
 
 	addEventListener("scroll", () => {
 		nav.style.background = scrollY > 0 ? "rgba(0, 0, 0, .33)" : "rgba(0, 0, 0, 0)"
